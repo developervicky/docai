e9e043395feeb13c54c0f5f7917aa8c9c08fee1a
developervicky <developer.vicky6@gmail.com>
2024-03-22 03:09:53 +0530

    refactor: plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 
-
 const config = {
   darkMode: ["class"],
   content: [
@@ -75,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
 
 export default config;
